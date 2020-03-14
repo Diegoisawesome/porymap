0eecfa00bf1e3164b5ab31ea0b8050e89fb6ee91
GriffinR <griffin.g.richards@gmail.com>
2020-03-14 03:44:55 -0400

    Allow editing border dimensions

--- include/core/map.h
+++ include/core/map.h
@@ -17,6 +17,10 @@
 #define DEFAULT_BORDER_WIDTH 2
 #define DEFAULT_BORDER_HEIGHT 2
 
+// Number of metatiles to draw out from edge of map. Could allow modification of this in the future.
+// porymap will reflect changes to it, but the value is hard-coded in the projects at the moment
+#define BORDER_DISTANCE 6
+
 class Map : public QObject
 {
     Q_OBJECT
@@ -83,12 +87,14 @@ public:
     void addEvent(Event*);
     QPixmap renderConnection(MapConnection, MapLayout *);
     QPixmap renderBorder();
-    void setDimensions(int newWidth, int newHeight, bool setNewBlockData = true);
+    void setDimensions(int newWidth, int newHeight, bool setNewBlockdata = true);
+    void setBorderDimensions(int newWidth, int newHeight, bool setNewBlockdata = true);
     void cacheBorder();
     bool hasUnsavedChanges();
 
 private:
     void setNewDimensionsBlockdata(int newWidth, int newHeight);
+    void setNewBorderDimensionsBlockdata(int newWidth, int newHeight);
 
 signals:
     void mapChanged(Map *map);
